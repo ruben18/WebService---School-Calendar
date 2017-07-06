3cb63e7e4020dab12429da85b88d61c7c3a789b5
Ruben Santos <ruben_18_95@hotmail.com>
2017-07-06 23:01:10 +0100

    Add jquery and changed index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html>
     <head>
+        <meta charset="utf-8">
+        
+        <title>Calendar WebService</title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="css/bootstrap.min.css" />
 
@@ -8,11 +11,19 @@
         <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 
         <!-- Latest compiled and minified JavaScript -->
+        <script src="js/jquery-3.2.1.min.js" ></script>
         <script src="js/bootstrap.min.js" ></script>
     </head>
     <body>
-        <div clas="container text-center">
-            <h1>Calendar WebService</h1>    
+        <div clas="container">
+            <div class="row text-center">
+                <h1>
+                    Calendar WebService
+                    <small><br/>
+                        If you have some questions, talk to me!
+                        <a href="https://github.com/ruben18/WebService---School-Calendar">Github Page</a></small>
+                </h1> 
+            </div>
         </div>
     </body>
 
