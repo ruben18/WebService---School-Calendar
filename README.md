bac19146df537af6b01c270ec6d48bbd043ce038
Ruben Santos <ruben_18_95@hotmail.com>
2017-07-06 22:46:09 +0100

    Remove Laravel framework too use only PHP

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Laravel PHP Web Service - School-Calendar
+PHP Web Service Calendar
 
-
-
-Web Service made with laravel to integrate with a android app.
+Web Service made with PHP to integrate with a android app.
 
 The webservice will be made to create a schedule.
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.min.js" ></script>
+    </head>
+    <body>
+        <div clas="container text-center">
+            <h1>Calendar WebService</h1>    
+        </div>
+    </body>
+
+        
+</html>
