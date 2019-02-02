e031e2503208fa71fb8d7a059fc2e3cd574cbd00
Ruben Santos <ruben_18_95@hotmail.com>
2019-02-02 17:02:59 +0000

    Validate requests without login

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Requests\TaskRequest;
 use App\Task;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,7 +10,7 @@ use App\Http\Resources\Task as TaskResource;
 
 class TaskController extends Controller
 {
-    public function create(Request $request){
+    public function create(TaskRequest $request){
        Task::create($request->all());
        return response()->json([
            'status'=>201
@@ -21,17 +22,32 @@ class TaskController extends Controller
         return TaskResource::collection(Task::where('user_id',$id)->get());
    }
 
-   public function update(Request $request, $id){
-       Task::findOrFail($id)->update($request->all());
+   public function update(TaskRequest $request, $id){
+       $task=Task::find($id);
+
+       if($task==null){
+           return response()->json(['status'=>404]);
+       }
+
+       $task->update($request->all());
        return response()->json([
            'status'=>200
        ]);
    }
 
     public function delete( $id){
-        Task::findOrFail($id)->delete();
+        $task=Task::find($id);
+
+        if($task==null){
+            return response()->json(['status'=>404]);
+        }
+
+        $task->delete();
+
         return response()->json([
             'status'=>200
         ]);
     }
+
+
 }
